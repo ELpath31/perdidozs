0df2198ed83547a6cd55ab59cc4328a5dc84fc08
ELpath31 <TU_CORREO_DE_GITHUB>
2026-03-17 19:04:38 -0500

    a

--- Assets/Lenguaje.js
+++ Assets/Lenguaje.js
@@ -1,13 +1,13 @@
-const audio = document.getElementById("bg-music");
-if (audio) {
-  audio.volume = 0.8; // volumen bajito (0 = mute, 1 = máximo)
-  audio.play().catch(() => {
-    window.addEventListener(
-      "click",
-      () => {
-        audio.play().catch(() => {});
-      },
-      { once: true }
-    );
-  });
-}
+  const audio = document.getElementById("bg-music");
+  if (audio) {
+    audio.volume = 0.8; // volumen bajito (0 = mute, 1 = máximo)
+    audio.play().catch(() => {
+      window.addEventListener(
+        "click",
+        () => {
+          audio.play().catch(() => {});
+        },
+        { once: true }
+      );
+    });
+  }
